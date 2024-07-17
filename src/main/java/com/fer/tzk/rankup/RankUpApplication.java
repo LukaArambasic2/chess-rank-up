e4e29cfeed795efd35441a11c8a12fbe73da8c56
Petar Štika <stika.petar@gmail.com>
2024-07-17 16:47:46 +0200

    Change name of project to TZK RankUp

--- src/main/java/com/fer/tzk/rankup/RankUpApplication.java
+++ src/main/java/com/fer/tzk/rankup/RankUpApplication.java
@@ -1,13 +1,13 @@
-package com.fer.chess_rank_up;
+package com.fer.tzk.rankup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChessRankUpApplication {
+public class RankUpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChessRankUpApplication.class, args);
+		SpringApplication.run(RankUpApplication.class, args);
 	}
 
 }
