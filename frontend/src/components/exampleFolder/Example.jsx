0bbbc9d28ce69ad83bbb6a55810c93eced9ba183
Luka Arambašić <luxarambas@pop-os.localdomain>
2024-07-21 13:14:20 +0200

    Added frontend

---
+++ frontend/src/components/exampleFolder/Example.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./Example.css";
+
+const Example = () => {
+    return (
+        <>
+            <div>This second div is in a component</div>
+        </>
+    );
+}
+
+export default Example;
