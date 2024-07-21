39c1e949a80eb6171245708605aa08a401c161c5
Petar Štika <stika.petar@gmail.com>
2024-07-22 01:34:48 +0200

    Add exception check

--- backend/src/test/java/hr/fer/tzk/rankup/model/MemberRepositoryTest.java
+++ backend/src/test/java/hr/fer/tzk/rankup/model/MemberRepositoryTest.java
@@ -4,10 +4,11 @@ import hr.fer.tzk.rankup.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
@@ -19,7 +20,7 @@ public class MemberRepositoryTest {
     @Test
     @DirtiesContext
     public void shouldAddMember() {
-        Member member = new Member("Ivan", "Horvat", "0036548430");
+        Member member = new Member("Hrvoje", "Horvat", "1234567890");
         memberRepository.save(member);
         assertNotNull(member.getId());
     }
@@ -27,18 +28,17 @@ public class MemberRepositoryTest {
     @Test
     @DirtiesContext
     public void shouldDeclineSecondMember() {
-        try {
-            Member member = new Member("Ivan", "Horvat", "0036548430");
-            memberRepository.save(member);
-            Member member2 = new Member("Ivan", "Horvat", "0036548430");
+        Member member1 = new Member("Hrvoje", "Horvat", "1234567890");
+        memberRepository.save(member1);
+        assertNotNull(member1.getId());
+
+        // Should throw an error because of not unique JMBAG
+        Member member2 = new Member("Ivan", "Ivanovic", "1234567890");
+
+        Exception exception = assertThrows(DataIntegrityViolationException.class, () -> {
             memberRepository.save(member2);
-            assertNotNull(member.getId());
+        });
 
-
-        } catch (Exception e) {
-            System.out.println("Success");
-        }
-
+       assertNull(member2.getId());
     }
-
 }
