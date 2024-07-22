14ab9feca19e6f8a2411df753b7ae05d062bae82
Petar Štika <stika.petar@gmail.com>
2024-07-22 04:32:43 +0200

    Add find by jmbag and email

--- backend/src/main/java/hr/fer/tzk/rankup/repository/MemberRepository.java
+++ backend/src/main/java/hr/fer/tzk/rankup/repository/MemberRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import hr.fer.tzk.rankup.model.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByJmbag(String jmbag);
+
+    Optional<Member> findByEmail(String email);
 }
