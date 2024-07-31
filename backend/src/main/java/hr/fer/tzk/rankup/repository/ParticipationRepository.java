122aa45cb42e9b4407c199225bcb9ce7819a29ef
Mila1308 <mpodrug1308@gmail.com>
2024-07-31 16:06:46 +0200

    Add participation repository

--- backend/src/main/java/hr/fer/tzk/rankup/repository/ParticipationRepository.java
+++ backend/src/main/java/hr/fer/tzk/rankup/repository/ParticipationRepository.java
@@ -7,5 +7,12 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParticipationRepository {
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    List<Participation> findByIdMember(Long idMember);
+
+    List<Participation> findByIdEvent(Long idEvent);
+
+    Optional<Participation> findByIdMemberAndIdEvent(Long idMember, Long idEvent);
+
 }
