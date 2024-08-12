3726e3762dc9f74f1b11aba8f03caa5cd2d911b8
Marin Čičak <cicakmarin4@gmail.com>
2024-08-12 21:25:28 +0200

    add NewsForm and update NewsMapper

--- backend/src/main/java/hr/fer/tzk/rankup/mapper/NewsMapper.java
+++ backend/src/main/java/hr/fer/tzk/rankup/mapper/NewsMapper.java
@@ -2,7 +2,12 @@ package hr.fer.tzk.rankup.mapper;
 
 import hr.fer.tzk.rankup.dto.BasicMemberDto;
 import hr.fer.tzk.rankup.dto.NewsDto;
+import hr.fer.tzk.rankup.form.NewsForm;
+import hr.fer.tzk.rankup.model.Member;
 import hr.fer.tzk.rankup.model.News;
+import hr.fer.tzk.rankup.model.Section;
+
+import java.time.LocalDate;
 
 public class NewsMapper {
     public static NewsDto toDto(News news) {
@@ -19,5 +24,18 @@ public class NewsMapper {
         newsDto.setDateEdited(news.getDateEdited());
 
         return newsDto;
+    }
+
+    public static News fromForm(NewsForm newsForm, Section section, Member member) {
+        News news = new News();
+        news.setTitle(newsForm.getTitle());
+        news.setContent(newsForm.getContent());
+        news.setSection(section);
+        news.setAuthor(member);
+        news.setImages(newsForm.getImages());
+        news.setDateCreated(LocalDate.now());
+        news.setDateEdited(null);
+
+        return news;
     }
 }
